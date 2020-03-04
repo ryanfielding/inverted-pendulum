7ea21e850cd3c63319892a928a0b07b242044ef0
Ryan Fielding <rafieldi@sfu.ca>
2020-03-03 17:37:20 -0800

    added ADC init, tested potentiometer PB4

--- tiva/main.c
+++ tiva/main.c
@@ -46,11 +46,11 @@ void PortF_Handler(void);
 
 //Other
 void MSDelay(unsigned int itime);
-
+void read_ADC(void);
 /* -----------------------      Global Variables        --------------------- */
 
 volatile signed long ComparatorValue = 5600;
-
+uint32_t pui32ADC0Value[1]; //data from ADC0
 
 /* -----------------------          Main Program        --------------------- */
 int main(void){
@@ -58,20 +58,21 @@ int main(void){
     PLL_Init(); //set CPU clock to 80MHz
     PortF_Init();
     PWM_Init();
-
+    ADC_Init();
     InitConsole();
     // Display the setup on the terminal, View > Terminal
-    UARTprintf("ADC ->\n");
+    UARTprintf("ADC0 ->\n");
     UARTprintf("  Type: Potentiometer\n");
     UARTprintf("  Samples: One\n");
-    UARTprintf("  Update Rate: 250ms\n");
+    UARTprintf("  Update Rate: 100ms\n");
 
 
     // Master interrupt enable func for all interrupts
     IntMasterEnable();
     enable_interrupts();
     while(1){
-
+        read_ADC();
+        MSDelay(100);
     }
 }
 
@@ -173,7 +174,51 @@ void MSDelay(unsigned int itime){
 
 
 void ADC_Init(void){
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); //enable ADC0
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); //enable GPIO B
+        GPIOPinTypeADC(GPIO_PORTB_BASE, GPIO_PIN_4); //PB4 as analog input
 
+        // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+        // will do a single sample when the processor sends a signal to start the
+        // conversion.  Each ADC module has 4 programmable sequences, sequence 0
+        // to sequence 3.  This example is arbitrarily using sequence 3.
+        ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+        // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
+        // single-ended mode (default) and configure the interrupt flag
+        // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
+        // that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
+        // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
+        // sequence 0 has 8 programmable steps.  Since we are only doing a single
+        // conversion using sequence 3 we will only configure step 0.  For more
+        // information on the ADC sequences and steps, reference the datasheet.
+        ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH10 | ADC_CTL_IE |
+                                 ADC_CTL_END);
+
+        // Since sample sequence 3 is now configured, it must be enabled.
+        ADCSequenceEnable(ADC0_BASE, 3);
+
+        // Clear the interrupt status flag.  This is done to make sure the
+        // interrupt flag is cleared before we sample.
+        ADCIntClear(ADC0_BASE, 3);
+
+}
+
+void read_ADC(void){
+    //Trigger the conversion
+    ADCProcessorTrigger(ADC0_BASE, 3);
+
+    // Wait for conversion to be completed.
+    while(!ADCIntStatus(ADC0_BASE, 3, false)){}
+
+    // Clear the ADC interrupt flag.
+    ADCIntClear(ADC0_BASE, 3);
+
+    // Read ADC Value.
+    ADCSequenceDataGet(ADC0_BASE, 3, pui32ADC0Value);
+
+    // Display the AIN10 (PB4) digital value on the console.
+    UARTprintf("PB4 = %4d\r", pui32ADC0Value[0],"\n");
 }
 
 
